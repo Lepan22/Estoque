7caa3c4a9b59376dcf91d27604c875b2bbe7c1e0
Lepan22 <contato@lepanpan.com.br>
2025-04-29 10:36:09 -0300

    Update firebase-config.js

--- assets/firebase-config.js
+++ assets/firebase-config.js
@@ -1,1 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBN-bmzgrlzjmrKMmuClZ8LVll-vJyx-aE",
+  authDomain: "controleestoquelepan.firebaseapp.com",
+  databaseURL: "https://controleestoquelepan-default-rtdb.firebaseio.com",
+  projectId: "controleestoquelepan",
+  storageBucket: "controleestoquelepan.firebasestorage.app",
+  messagingSenderId: "779860276544",
+  appId: "1:779860276544:web:f45844571a8c0bab1576a5",
+  measurementId: "G-EDKYH7TKMG"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
 
